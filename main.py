4b2dc92a114723756e3520b9228b0ea374e3ecc0
Facundo <acosta.facundo.h@gmail.com>
2020-08-18 16:42:16 -0300

    se creo el archivo main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Bienvenido")
+
+a = int(input("ingrese un numero:"))
+
+b = int(input("ingrese otro numero:"))
+
+suma = a + b
+resta = a - b
+divi = a / b
+
+print("la suma es",suma)
+print("la resta es",resta)
+print("la division es",divi)
