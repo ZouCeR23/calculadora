debc80975db23ec6357f2f3f9c270187d28e3ebc
ZouCeR23 <69871078+ZouCeR23@users.noreply.github.com>
2020-08-18 16:50:56 -0300

    agregamos la multiplicación

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ b = int(input("ingrese otro numero:"))
 suma = a + b
 resta = a - b
 divi = a / b
-
+multi = a * b
 print("la suma es",suma)
 print("la resta es",resta)
 print("la division es",divi)
+print("la multiplicacion es",multi)
